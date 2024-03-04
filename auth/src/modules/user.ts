ff041237ba9922a3b22b643fe995763a7d4ba319
Lateema Spencer <spencer.lateema@gmail.com>
2024-03-04 17:35:41 -0500

    added user model for mongoose

---
+++ auth/src/modules/user.ts
@@ -0,0 +1,53 @@
+import mongoose, { mongo } from 'mongoose';
+
+// An interface that describes the properties
+// that are required to create a new user
+/*
+  Describe what it takes to create a user
+*/
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that the user model has
+/*
+  Describes what the entire collection of user look
+  like
+  or at least
+  Methods associated with the user model
+*/
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a user document has
+/*
+  Describes what properties a single user has
+*/
+interface UserDoc extends mongoose.Document {
+  // an instance of a user has
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
